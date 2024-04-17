9a7e55c8029f0dfbfc47c34bc3cbded629697322
Akhil Kamalesh <akhilkamalesh@Akhils-MacBook-Pro.local>
2024-04-17 10:11:44 -0400

    Merged changes with Faizans

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import json
+from hashlib import sha256
 
 # Connect to MySQL (change password)
 def connectdatabase():
@@ -19,134 +20,94 @@ def querydb(querystr, mydb):
     #     print(row)
     return mycursor.fetchall()
 
-def authenticate(mydb, uid, lastname):
-     mycursor = mydb.cursor(dictionary=True)
-     print(uid)
-     mycursor.execute("select * from patients where Patient_ID = %s and Last_Name = %s", (uid, lastname))
-     for row in mycursor.fetchall():
-         print(row)
-         if row['Last_Name'] == 'admin':
-             print('admin ind')
-             return 'Admin'
-         else:
-             print('pat')
-             return 'Patient'
-         
-     return 'None'
-
-
+def authenticate(mydb, uid, password):
+    mycursor = mydb.cursor(dictionary=True)
+    hashed_password = sha256(password.encode()).hexdigest()
+    print(hashed_password)
+    mycursor.execute("SELECT * FROM users WHERE uid = %s AND password = %s", (uid, hashed_password))
+    print(mycursor.fetchone())
+    return bool(mycursor.fetchone()) 
 
 def get_user_info(mydb, uid):
     mycursor = mydb.cursor(dictionary=True)
-    print(uid)
-    mycursor.execute("select p.Patient_ID, p.First_Name as Patient_Name, p.age, d.*, h.*, m.MedRecID, m.COVID_Positive, m.Vaccine, m.positive_date, m.vaccination_date from patients p join medical_records m on p.Patient_ID = m.patient_id\
-                     join doctors d on p.doctor_id = d.doctor_id \
-                     join hospitals h on d.hospital_id = h.hospital_id where p.patient_id =  %s", (uid, ))
-    for row in mycursor.fetchall():
-        print(row)
+    mycursor.execute("SELECT p.Patient_ID, p.First_Name AS Patient_Name, p.age, d.*, h.*, m.MedRecID, m.COVID_Positive, m.Vaccine "
+                     "FROM patients p "
+                     "JOIN medical_records m ON p.Patient_ID = m.patient_id "
+                     "JOIN doctors d ON p.doctor_id = d.doctor_id "
+                     "JOIN hospitals h ON d.hospital_id = h.hospital_id "
+                     "WHERE p.patient_id = %s", (uid,))
+    return mycursor.fetchone()
 
-    return row
-
-
+def signup_user(mydb, uid, last_name, password):
+    mycursor = mydb.cursor()
+    hashed_password = sha256(password.encode()).hexdigest()
+    mycursor.execute("INSERT INTO users (uid, last_name, password) VALUES (%s, %s, %s)", (uid, last_name, hashed_password))
+    mydb.commit()
+    return get_user_info(mydb, uid)
 
 def createentity(mydb, data):
-    data_string = data.decode('utf-8')
-    data_dict = json.loads(data_string)
-    print(data_dict)
+    data_dict = json.loads(data)
     mycursor = mydb.cursor()
 
     if data_dict['type'] == 'patient':
-
-        if(data_dict['patientDoctorID'] == ''):
-            mycursor.execute("insert into `patients` values (%s, %s, %s, %s, NULL)", (data_dict['patientUID'],
-                                                                                data_dict['patientFirstname'],
-                                                                                data_dict['patientLastname'],
-                                                                                data_dict['patientAge']))
-            mycursor.execute("select * from `patients`")
-            for row in mycursor.fetchall():
-                print(row)
+        if not data_dict['patientDoctorID']:
+            mycursor.execute("INSERT INTO `patients` VALUES (%s, %s, %s, %s, NULL)", (data_dict['patientUID'], data_dict['patientFirstname'], data_dict['patientLastname'], data_dict['patientAge']))
         else:
-            mycursor.execute("insert into `patients` values (%s, %s, %s, %s, %s)", (data_dict['patientUID'],
-                                                                                data_dict['patientFirstname'],
-                                                                                data_dict['patientLastname'],
-                                                                                data_dict['patientAge'],
-                                                                                data_dict['patientDoctorID']))
+            mycursor.execute("INSERT INTO `patients` VALUES (%s, %s, %s, %s, %s)", (data_dict['patientUID'], data_dict['patientFirstname'], data_dict['patientLastname'], data_dict['patientAge'], data_dict['patientDoctorID']))
     elif data_dict['type'] == 'doctor':
-
-        if(data_dict['doctorHospitalID'] == ''):
-            mycursor.execute("insert into `doctors` values (%s, %s, %s, NULL)", (data_dict['doctorId'],
-                                                                                data_dict['doctorFirstname'],
-                                                                                data_dict['doctorLastname']))
-            mycursor.execute("select * from `doctors`")
-            for row in mycursor.fetchall():
-                print(row)
+        if not data_dict['doctorHospitalID']:
+            mycursor.execute("INSERT INTO `doctors` VALUES (%s, %s, %s, NULL)", (data_dict['doctorId'], data_dict['doctorFirstname'], data_dict['doctorLastname']))
         else:
-            mycursor.execute("insert into `doctors` values (%s, %s, %s, %s)", (data_dict['doctorId'],
-                                                                                data_dict['doctorFirstname'],
-                                                                                data_dict['doctorLastname'],
-                                                                                data_dict['doctorHospitalID']))    
+            mycursor.execute("INSERT INTO `doctors` VALUES (%s, %s, %s, %s)", (data_dict['doctorId'], data_dict['doctorFirstname'], data_dict['doctorLastname'], data_dict['doctorHospitalID']))
     elif data_dict['type'] == 'hospital':
-
-        if(data_dict['hospitalCountryID'] == ''):
-
-            mycursor.execute("insert into `hospitals` values (%s, NULL, %s, %s, %s)", (data_dict['hospitalID'],
-                                                                                data_dict['hospitalName'],
-                                                                                data_dict['hospitalGeolocation'],
-                                                                                data_dict['hospitalCapacity']))  
+        if not data_dict['hospitalCountryID']:
+            mycursor.execute("INSERT INTO `hospitals` VALUES (%s, NULL, %s, %s, %s)", (data_dict['hospitalID'], data_dict['hospitalName'], data_dict['hospitalGeolocation'], data_dict['hospitalCapacity']))
         else:
-
-            mycursor.execute("insert into `hospitals` values (%s, %s, %s, %s, %s)", (data_dict['hospitalID'],
-                                                                                    data_dict['hospitalCountryID'],
-                                                                                    data_dict['hospitalName'],
-                                                                                    data_dict['hospitalGeolocation'],
-                                                                                    data_dict['hospitalCapacity'])) 
-
+            mycursor.execute("INSERT INTO `hospitals` VALUES (%s, %s, %s, %s, %s)", (data_dict['hospitalID'], data_dict['hospitalCountryID'], data_dict['hospitalName'], data_dict['hospitalGeolocation'], data_dict['hospitalCapacity']))
     mydb.commit()
     return 'complete'
 
 def deleteentity(mydb, data):
-    data_string = data.decode('utf-8')
-    data_dict = json.loads(data_string)
-    print(data_dict)
+    data_dict = json.loads(data)
     mycursor = mydb.cursor()
-
     if data_dict['type'] == 'patient':
-        mycursor.execute('DELETE from `patients` where Patient_ID = (%s)', (data_dict['patientID'], ))
-
+        mycursor.execute('DELETE FROM `patients` WHERE Patient_ID = %s', (data_dict['patientID'],))
     elif data_dict['type'] == 'doctor':
-        mycursor.execute('DELETE from `doctors` where Doctor_ID = (%s)', (data_dict['doctorID'], ))
-    
+        mycursor.execute('DELETE FROM `doctors` WHERE Doctor_ID = %s', (data_dict['doctorID'],))
     elif data_dict['type'] == 'hospital':
-        mycursor.execute('DELETE from `hospitals` where Hospital_ID = (%s)', (data_dict['hospitalId'], ))
-
+        mycursor.execute('DELETE FROM `hospitals` WHERE Hospital_ID = %s', (data_dict['hospitalId'],))
     mydb.commit()
     return 'complete'
 
-
 def updateentity(mydb, data):
-   data_string = data.decode('utf-8')
-   data_dict = json.loads(data_string)
-   print(data_dict)
-   mycursor = mydb.cursor()
+    data_dict = json.loads(data)
+    mycursor = mydb.cursor()
+    if data_dict['type'] == 'patient':
+        mycursor.execute('UPDATE patients SET First_Name = %s, Last_Name = %s, Age = %s, Doctor_ID = %s WHERE Patient_ID = %s', (data_dict['patientFirstname'], data_dict['patientLastname'], data_dict['patientAge'], data_dict['patientDoctorID'], data_dict['patientUID']))
+    elif data_dict['type'] == 'doctor':
+        mycursor.execute('UPDATE doctors SET First_Name = %s, Last_Name = %s WHERE Doctor_ID = %s', (data_dict['doctorFirstname'], data_dict['doctorLastname'], data_dict['doctorId']))
+    elif data_dict['type'] == 'hospital':
+        mycursor.execute('UPDATE hospitals SET Name = %s, Geolocation = %s, Capacity = %s WHERE Hospital_ID = %s', (data_dict['hospitalName'], data_dict['hospitalGeolocation'], data_dict['hospitalCapacity'], data_dict['hospitalId']))
+    mydb.commit()
+    return 'complete'
 
-   if data_dict['type'] == 'patient':
-       mycursor.execute('UPDATE patients SET patientFirstname = (%s), patientLastname = (%s), patientAge = (%s), patientDoctorID = (%s) WHERE user_account.name = Patient_ID = (%s)', (data_dict['patientFirstname'], 
-                                                                               data_dict['patientLastname'],
-                                                                               data_dict['patientAge'],
-                                                                               data_dict['patientDoctorID'], 
-                                                                               data_dict['patientUID']))
+def user_exists(mydb, uid, lastname):
+    mycursor = mydb.cursor(dictionary=True)
+    mycursor.execute("SELECT * FROM users WHERE uid = %s AND last_name = %s", (uid, lastname))
+    return bool(mycursor.fetchone())
 
-   elif data_dict['type'] == 'doctor':
-       mycursor.execute('UPDATE doctors SET doctorFirstname = (%s), doctorLastname = (%s) WHERE user_account.name = Doctor_ID = (%s)', (data_dict['doctorFirstname'], 
-                                                                               data_dict['doctorLastname'],
-                                                                               data_dict['doctorId']))
-   
-   elif data_dict['type'] == 'hospital':
-       mycursor.execute('UPDATE doctors SET hospitalName = (%s), hospitalGeolocation = (%s), hospitalCapacity = (%s) WHERE user_account.name = Hospital_ID = (%s)',
-                                                                                   data_dict['hospitalName'],
-                                                                                   data_dict['hospitalGeolocation'],
-                                                                                   data_dict['hospitalCapacity'],
-                                                                                   data_dict['hospitalId'])
 
-   mydb.commit()
-   return 'complete'
+def get_doctor_info(mydb, uid):
+    mycursor = mydb.cursor(dictionary=True)
+    print(uid)
+    patients = []
+    doctor = []
+    mycursor.execute("select p.First_Name, p.Last_Name, p.age from patients p where Doctor_ID = %s", (uid, ))
+    for row in mycursor.fetchall():
+        patients.append(row)
+    
+    mycursor.execute("select * from doctors d join hospitals h on d.Hospital_ID = h.Hospital_ID")
+    for row in mycursor.fetchall():
+        doctor.append(row)
+
+    return {'patients': patients, 'doctors': doctor}
--- server.py
+++ server.py
@@ -1,73 +1,185 @@
-from flask import Flask, render_template, request
-import db as db
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect, url_for, session
+import mysql.connector
+import db as database_module
 
 app = Flask(__name__, template_folder='pages')
 
-mydb = db.connectdatabase()
+mydb = database_module.connectdatabase()
 
 session = {}
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/', methods=['POST'])
-def submit():
-  if request.method == 'POST':
-        uid = request.form['uid']
-        lastname = request.form['lastname']
-        if db.authenticate(mydb, uid, lastname) == 'None':
-           print('invalid')
-           return index()
-        elif db.authenticate(mydb, uid, lastname) == 'Patient':
-           session['uid'] = uid
-           session['lastname'] = lastname
-           print('works')
-           return portal()
-        elif db.authenticate(mydb, uid, lastname) == 'Admin':
-           print('works')
-           return admin()
-        
+@app.route('/login', methods=['POST'])
+def login():
+    uid = request.form['uid']
+    password = request.form['password']
+    if database_module.authenticate(mydb, uid, password):
+        session['uid'] = uid
+        return redirect(url_for('portal'))
+    else:
+        return "Login Failed", 401
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    uid = request.form['uid']
+    lastname = request.form['lastname']
+    password = request.form['password']
+    
+    if database_module.user_exists(mydb, uid, lastname):
+        return "This user already has an account", 409
+    try:
+        if database_module.signup_user(mydb, uid, lastname, password):
+            session['uid'] = uid
+            return redirect(url_for('portal'))
+        else:
+            return "Signup Failed", 400
+    except mysql.connector.IntegrityError as e:
+        return f"THIS USER ALREADY HAS AN ACCOUNT. PLEASE SIGN IN", 500
+
 @app.route('/portal', methods=['GET'])
 def portal():
-   if 'uid' in session:
-      uid = session['uid']
-      user_info = db.get_user_info(mydb, uid)
-      print(user_info)
-      if user_info['Vaccine'] == 1:
-         user_info['Vaccine'] = 'True'
-      else:
-         user_info['Vaccine'] = 'False'
-
-      if user_info['COVID_Positive'] == 1:
-         user_info['COVID_Positive'] = 'True'
-      else:
-         user_info['COVID_Positive'] = 'False'
-
-      return render_template('portal.html', user_info = user_info)
+    if 'uid' in session:
+        uid = session['uid']
+        user_info = database_module.get_user_info(mydb, uid)
+        if not user_info:
+            return "No user information available", 404
+        return render_template('portal.html', user_info=user_info)
+    return redirect(url_for('index'))
 
 @app.route('/admin', methods=['GET'])
 def admin():
-   return render_template('admin.html')
-
-@app.route('/admin', methods=['POST'])
-def createentity():
-   print(request.data)
-   db.createentity(mydb, request.data)
-   return render_template('admin.html')
-
-@app.route('/admin', methods=['DELETE'])
-def deleteentity():
-   print(request.data)
-   db.deleteentity(mydb, request.data)
-   return render_template('admin.html')
-
-# @app.route('/admin/createdoctor', methods=['POST'])
-# def createdoctor():
-#    db.createdoctor(mydb, request.data)
-#    return render_template('admin.html')
-
+    return render_template('admin.html')
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from flask import Flask, render_template, request
+# import db as db
+# app = Flask(__name__)
+
+# app = Flask(__name__, template_folder='pages')
+
+# mydb = db.connectdatabase()
+
+# session = {}
+
+# @app.route('/')
+# def usertype():
+#    return render_template('index.html')
+
+# @app.route('/patientlogin')
+# def patientlogin():
+#   return render_template('patientlogin.html')
+
+# @app.route('/patientlogin', methods=['POST'])
+# def submit():
+#   if request.method == 'POST':
+#         uid = request.form['uid']
+#         lastname = request.form['lastname']
+#         if db.authenticate(mydb, uid, lastname) == 'None':
+#            print('invalid')
+#            return patientlogin()
+#         elif db.authenticate(mydb, uid, lastname) == 'Patient':
+#            session['uid'] = uid
+#            session['lastname'] = lastname
+#            print('works')
+#            return portal()
+#         elif db.authenticate(mydb, uid, lastname) == 'Admin':
+#            print('works')
+#            return admin()
+        
+# @app.route('/doctorlogin')
+# def doctorlogin():
+#    return render_template('doctorlogin.html')
+
+
+# @app.route('/doctorlogin', methods=['POST'])
+# def dsubmit():
+#    if request.method == 'POST':
+#         uid = request.form['uid']
+#         lastname = request.form['lastname']
+#         if db.dauthenticate(mydb, uid, lastname) == 'None':
+#             print('invalid')
+#             return doctorlogin()
+#         elif db.dauthenticate(mydb, uid, lastname) == 'True':
+#             session['uid'] = uid
+#             session['lastname'] = lastname
+#             print('works')
+#             return doctorportal()
+        
+
+# @app.route('/portal', methods=['GET'])
+# def portal():
+#    if 'uid' in session:
+#       uid = session['uid']
+#       user_info = db.get_user_info(mydb, uid)
+#       print(user_info)
+#       if user_info['Vaccine'] == 1:
+#          user_info['Vaccine'] = 'True'
+#       else:
+#          user_info['Vaccine'] = 'False'
+
+#       if user_info['COVID_Positive'] == 1:
+#          user_info['COVID_Positive'] = 'True'
+#       else:
+#          user_info['COVID_Positive'] = 'False'
+
+#       return render_template('portal.html', user_info = user_info)
+   
+
+# @app.route('/doctorportal', methods=['GET'])
+# def doctorportal():
+#    if 'uid' in session:
+#       uid = session['uid']
+#       doctor_info = db.get_doctor_info(mydb, uid)
+#       return render_template('doctorportal.html', doctor_info=doctor_info)
+
+
+# @app.route('/admin', methods=['GET'])
+# def admin():
+#    return render_template('admin.html')
+
+# @app.route('/admin', methods=['POST'])
+# def createentity():
+#    print(request.data)
+#    db.createentity(mydb, request.data)
+#    return render_template('admin.html')
+
+# @app.route('/admin', methods=['DELETE'])
+# def deleteentity():
+#    print(request.data)
+#    db.deleteentity(mydb, request.data)
+#    return render_template('admin.html')
+
+# if __name__ == '__main__':
+#   app.run(debug=True)
